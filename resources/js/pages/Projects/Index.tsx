74de0ebbc1b73ddea375900adf2eea4a291f6742
ardianilyas <ardianilyas@gmail.com>
2025-05-13 12:46:42 +0700

    style: add card component

--- resources/js/pages/Projects/Index.tsx
+++ resources/js/pages/Projects/Index.tsx
@@ -10,6 +10,7 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import { Project } from '@/types/project';
+import Card from '@/components/Card';
 
 interface Props {
     projects: Project[]
@@ -21,31 +22,33 @@ export default function Index({ projects }: Props) {
         <>
             <Head title="Projects" />
             <AppLayout title='Projects' desc={desc}>
-                <Table>
-                    <TableCaption>Project list</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead>No</TableHead>
-                            <TableHead>Name</TableHead>
-                            <TableHead>Description</TableHead>
-                            <TableHead>Members</TableHead>
-                            <TableHead>Tasks</TableHead>
-                            <TableHead>Created at</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {projects.map((project, index) => (
-                            <TableRow key={project.id}>
-                                <TableCell>{index + 1}</TableCell>
-                                <TableCell>{project.name}</TableCell>
-                                <TableCell>{project.description}</TableCell>
-                                <TableCell>{project.users_count}</TableCell>
-                                <TableCell>{project.tasks_count}</TableCell>
-                                <TableCell>{project.created_at}</TableCell>
+                <Card>
+                    <Table>
+                        <TableCaption>Project list</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>No</TableHead>
+                                <TableHead>Name</TableHead>
+                                <TableHead>Description</TableHead>
+                                <TableHead>Members</TableHead>
+                                <TableHead>Tasks</TableHead>
+                                <TableHead>Created at</TableHead>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
+                        </TableHeader>
+                        <TableBody>
+                            {projects.map((project, index) => (
+                                <TableRow key={project.id}>
+                                    <TableCell>{index + 1}</TableCell>
+                                    <TableCell>{project.name}</TableCell>
+                                    <TableCell>{project.description}</TableCell>
+                                    <TableCell>{project.users_count}</TableCell>
+                                    <TableCell>{project.tasks_count}</TableCell>
+                                    <TableCell>{project.created_at}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Card>
             </AppLayout>
         </>
     )
