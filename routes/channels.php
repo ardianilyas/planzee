d3c57cd289af819d49409d611cb1ad5182c4086b
ardianilyas <ardianilyas@gmail.com>
2025-05-23 09:53:35 +0700

    style: remove online-users channel

--- routes/channels.php
+++ routes/channels.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-
-Broadcast::channel('online-users', function ($user) {
-    return ['id' => $user->id, 'name' => $user->name];
-});
