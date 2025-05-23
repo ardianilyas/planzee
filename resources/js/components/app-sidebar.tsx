8f79db6882422d7ea47ab11db021647d5a39dda6
ardianilyas <ardianilyas@gmail.com>
2025-05-23 14:45:18 +0700

    style: change sidebar icon

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -3,7 +3,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { LayoutGrid } from 'lucide-react';
+import { LayoutGrid, SquareKanban } from 'lucide-react';
 import AppLogo from './app-logo';
 
 
@@ -20,7 +20,7 @@ export function AppSidebar() {
         {
             title: 'Projects',
             href: route('dashboard.projects.index'),
-            icon: LayoutGrid,
+            icon: SquareKanban,
             isActive: url.startsWith('/dashboard/projects'),
         },
     ];
